779f3dc18b41f0d24b6fa033f42f1ebdff8c2ae4
Alexander_Myachin <Alexander_Myachin@corvel.com>
2019-06-03 14:45:04 -0700

    More changes

--- StateMachines/StateMachine.cs
+++ StateMachines/StateMachine.cs
@@ -87,51 +87,6 @@ namespace StateMachines
         public object ErrorStatus { get; }
     }
 
-    public class ListFragment<T> : IReadOnlyList<T>
-    {
-        public ListFragment(IEnumerable<T> items, bool moreDataPending)
-        {
-            Items = new List<T>(items);
-            MoreDataPending = moreDataPending;
-        }
-
-        public ListFragment(IEnumerable<T> items, int capacity)
-        {
-            Items = new List<T>(items);
-            MoreDataPending = Items.Count >= capacity;
-        }
-
-        public bool MoreDataPending { get; }
-
-        private IReadOnlyList<T> Items { get; }
-
-        public int Count => Items.Count;
-
-        public T this[int index] => Items[index];
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return Items.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return Items.GetEnumerator();
-        }
-    }
-
-    public static class ListFragmentExtensions
-    {
-        public static ListFragment<TActor> ToFragment<TActor>(this IEnumerable<TActor> items, bool moreData)
-        {
-            return new ListFragment<TActor>(items, moreData);
-        }
-        public static ListFragment<TActor> ToFragment<TActor>(this IEnumerable<TActor> items, int capacity)
-        {
-            return new ListFragment<TActor>(items, capacity);
-        }
-
-    }
 
     public abstract class StateMachine<TActor, TStatus> 
         where TActor : Actor<TStatus>
